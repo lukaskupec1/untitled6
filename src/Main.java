2d0c5e6804d0291d226cbbd85d1ea6d6feb0931e
lukaskupec1 <ghost.007@email.cz>
2023-01-16 21:31:41 +0100

    hotovo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        zaznamZakazniku zaznam = new zaznamZakazniku();
+        Zakaznik zakaznik1 = new Zakaznik("Martin Boček", 50);
+        Zakaznik zakaznik2 = new Zakaznik("Petr Křivý", 45);
+        zaznamZakazniku.nactiZeSouboru("vstup.txt");
+        for(Zakaznik zakaznik : zaznam.getList()){ System.out.println(zakaznik.getJmeno()+":"+zakaznik.getPocetProdeju()+"\n");         }
+        zaznam.pridat(zakaznik1);         zaznam.pridat(zakaznik2);         zaznam.zapisDoSouboru("vystup.txt");     } }
+
